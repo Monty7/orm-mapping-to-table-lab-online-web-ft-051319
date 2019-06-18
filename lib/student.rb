1c9c4548790801d861b3ded69fbf58e4aadb5da3
M Banks <montensia1@outlook.com>
2019-06-18 17:47:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,12 @@ attr_reader :id
     DB[:conn].execute(sql)
   end
   
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) VALUES (?, ?)
